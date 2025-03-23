a8aa05e387f494deb79fd79101562ec25799a0ad
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-03-24 01:20:17 +0300

    seding data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+      .then(response => response.json()) 
+      .then(data => {
+        
+        const id = data.id; 
+        const newIdElement = document.createElement("p");
+        newIdElement.textContent = `New ID: ${id}`;
+        document.body.appendChild(newIdElement);
+      })
+      .catch(error => {
+        
+        const errorMessage = error.message;
+        const errorElement = document.createElement("p");
+        errorElement.textContent = `Error: ${errorMessage}`;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
